a97289338745cdf183ed9b29784db8539d27d34f
mohdamaan8954 <mohdammu086@gmail.com>
2026-09-17 20:52:33 -0700

    update

--- main.py
+++ main.py
@@ -1,33 +1,33 @@
 
 
-# # df = pd.read_csv("netflix_titles.csv")
-
-# # drop column and row not needed
-# df = df.drop(columns=['description'])
-
-# df = df.dropna(subset=['date_added',  'rating' , 'duration'])
-
-# #  fillna values with 'Unknown' for specific columns
-# df['director'] = df['director'].fillna('Unknown')
-# df['cast'] = df['cast'].fillna('Unknown')
-# df['country'] = df['country'].fillna('Unknown')
-
-# df['cast'] = df['cast'].str.replace(', ', '; ')
-# df['director'] = df['director'].str.replace(', ', '; ')
-# df['country'] = df['country'].str.replace(', ', '; ')
-# df['listed_in'] = df['listed_in'].str.replace(', ', '; ')
-
-# df['date_added'] = df['date_added'].str.strip()
-
-# df['date_added'] = pd.to_datetime(df['date_added'])
-
-# df.to_csv("netflix_cleaned.csv", index=False)
-
-
-
 import pandas as pd
 from sqlalchemy import create_engine
 from urllib.parse import quote_plus
+
+
+df = pd.read_csv("netflix_titles.csv")
+
+# drop column and row not needed
+df = df.drop(columns=['description'])
+
+df = df.dropna(subset=['date_added',  'rating' , 'duration'])
+
+#  fillna values with 'Unknown' for specific columns
+df['director'] = df['director'].fillna('Unknown')
+df['cast'] = df['cast'].fillna('Unknown')
+df['country'] = df['country'].fillna('Unknown')
+
+df['cast'] = df['cast'].str.replace(', ', '; ')
+df['director'] = df['director'].str.replace(', ', '; ')
+df['country'] = df['country'].str.replace(', ', '; ')
+df['listed_in'] = df['listed_in'].str.replace(', ', '; ')
+
+df['date_added'] = df['date_added'].str.strip()
+
+df['date_added'] = pd.to_datetime(df['date_added'])
+
+df.to_csv("netflix_cleaned.csv", index=False)
+
 
 password = quote_plus('@amaan895463')  # apna poora actual password daalo
 username = 'root'
